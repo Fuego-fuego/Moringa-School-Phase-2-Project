e73beb1ce766aa2530454eadbd8823da301ec844
nathaniEL mujesia <nathanielmujesia@gmail.com>
2024-02-15 12:39:17 +0300

    jsx css done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,15 @@
+import Main from './main'
+import Hero from './hero'
+import Footer from './footer'
+import '../App.css'
 
-import './App.css'
-
-function App() {
-
-
-  return (
+function App() {  return (
     <>
-
+      <Hero/>
+      <Main/>
+      <Footer/>
     </>
   )
 }
 
-export default App
+export default App;
