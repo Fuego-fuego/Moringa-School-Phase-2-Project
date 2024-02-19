a3a8ef7c109d9423ff9358bfd85ebb83d966b723
nathaniEL mujesia <nathanielmujesia@gmail.com>
2024-02-19 09:29:40 +0300

    Fire base authentication

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,27 @@
-import {useState,useEffect} from 'react'
+// import React, {useState, useEffect} from 'react';
+import Home from './Home';
+import Signup from './Signup';
+import Login from './Login';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
-import Main from './main'
-import Hero from './hero'
-import Footer from './footer'
-import Cart from './Cart'
+import '../App.css';
 
-import '../App.css'
-
-function App() {  
-const [openedCart , setOpenedCart] = useState(false);
-const [cartItems,setCartItems] = useState([]);
-
-
-useEffect(() => {
-  fetch(`http://localhost:8001/cart`)
-  .then(resp => resp.json())
-  .then(jewels => setCartItems(jewels))
-},[])
-
-// Add product to shopping cart
-function handleAddToCart(newCartItem){
-
-  setCartItems([...cartItems,newCartItem])
-}
-
-
-
-
-
-
-  
-  return (
-    <>
-      <Hero setOpenedCart={setOpenedCart}/>
-      <Main onAddToCart ={handleAddToCart}/>
-      <Cart openedCart ={openedCart} setOpenedCart={setOpenedCart} cartItems={cartItems}/>
-      <Footer/>
-    </>
-  )
+function App() {
+    return (
+        <Router>
+            <div>                
+                    <section>
+                        <Routes>
+                            <Route path="/home" element={<Home />} />
+                            <Route path="/signup" element={<Signup />} />
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/" element={<Login />} />
+                        </Routes>
+                    </section>               
+            </div>
+        </Router>
+    );
 }
 
 export default App;
